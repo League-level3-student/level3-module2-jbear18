1b0842ab9aa64b130cdb20bf6c481f3cab3c9034
jbear18 <shehu01@hotmail.com>
2020-04-10 18:33:19 -0700

    SortedArrayChecker, SwappingDigits(IP)
    finished sorted array checker
    swapping digits: work on bubble sorter

--- src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
+++ src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
@@ -7,21 +7,43 @@ public class _00_SortedArrayChecker {
 	//   The method returns true if the integer
 	//   array is in ascending order and false otherwise
 
-	
+	public static boolean intArraySorted(int[] sorted) {
+		for (int i = 0; i < sorted.length-1; i++) { 
+		if(sorted[i]>sorted[i+1]) {
+			return false;
+		}
+		
+	}
+		return true;
+	}
 	//2. Write a static method called doubleArraySorted. 
 	//   This method takes in an array of doubles
 	//   and it returns a boolean.
 	//   The method returns true if the double
 	//   array is in ascending order and false otherwise
 
-	
+	public static boolean doubleArraySorted(double[] a) {
+		for (int i = 0; i < a.length-1; i++) {
+			if(a[i]>a[i+1]) {
+				return false;
+			}
+		}
+	return true;	
+	}
 	//3. Write a static method called charArraySorted. 
 	//   This method takes in an array of characters
 	//   and it returns a boolean.
 	//   The method returns true if the character
 	//   array is in alphabetical order and false otherwise
 	//   (You can compare characters just like integers)
-
+public static boolean charArraySorted(char[]alpha) {
+	for (int i = 0; i < alpha.length-1; i++) {
+		if(alpha[i]>alpha[i+1]) {
+			return false;
+		}
+	}
+	return true;
+}
 	
 	//4. Write a static method called stringArraySorted. 
 	//   This method takes in an array of Strings
@@ -29,5 +51,12 @@ public class _00_SortedArrayChecker {
 	//   The method returns true if the String
 	//   array is in alphabetical order and false otherwise
 	//   (Use the compareTo(String) method)
-
+public static boolean stringArraySorted(String[] strings) {
+	for (int i = 0; i < strings.length-1; i++) {
+		if(strings[i].compareTo(strings[i+1])>0) {
+		return false;	
+		}
+	}
+	return true;
+}
 }
