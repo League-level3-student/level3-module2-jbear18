65c3b2a257edbec184aed9395051793372761cc9
jbear18 <shehu01@hotmail.com>
2020-06-05 18:31:58 -0700

    SearchTest complete!

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -11,24 +11,35 @@ class _04_SearchTest {
 	@Test
 	public void testLinearSearch() {
 		//1. use the assertEquals method to test your linear search method.
-
+String[] arr= {"banana", "apple", "fruit", "grapes"};
+assertEquals(1,_00_LinearSearch.linearSearch(arr, "apple"));
+assertEquals(-1, _00_LinearSearch.linearSearch(arr, "water"));
 	}
 
 	@Test
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		int[]arr= {1,2,3,5,8};
+		assertEquals(3,_01_BinarySearch.binarySearch(arr, 0, 4, 5));
+		assertEquals(-1, _01_BinarySearch.binarySearch(arr, 0, 4, 10));
 	}
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
+		int[]arr= {1,2,3,4};
+		assertEquals(2, _02_InterpolationSearch.interpolationSearch(arr, 3));
+		assertEquals(-1, _02_InterpolationSearch.interpolationSearch(arr, 9));
 	}
 	
 	@Test
 	public void testExponentialSearch() {
 		//4. use the assertEquals method to test your exponential search method.
 		//   remember that the array must be sorted
+		int[]arr= {1,2,3,4};
+		assertEquals(2, _03_ExponentialSearch.exponentialSearch(arr, 3));
+		assertEquals(-1, _03_ExponentialSearch.exponentialSearch(arr, 9));
 	}
 }
