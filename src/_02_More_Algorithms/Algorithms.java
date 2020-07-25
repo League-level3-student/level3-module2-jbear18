b27d55aba47d4b40464c42df0c0d5bb900aab750
jbear18 <shehu01@hotmail.com>
2020-07-24 18:37:33 -0700

    Algorithms (IP)

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -2,6 +2,8 @@ package _02_More_Algorithms;
 
 import java.util.List;
 
+import _00_Sorting_Algorithms.SortingVisualizer;
+
 public class Algorithms {
     public static int findBrokenEgg(List<String> eggs) {
         for(int i=0; i<eggs.size();i++) {
@@ -31,16 +33,48 @@ public static double findTallest(List<Double> peeps) {
     return tallest;    
 }
 
-//public static Object findLongestWord(List<String> words) {
-//    String word= "";
-//    for(int i=0; i<words.size(); i++) {
-//    if(words.get(i)>word) {
-//        word=words.get(i);
-//    }
-//    }
-//    return word;
-//}
+public static Object findLongestWord(List<String> words) {
+  String longestWord=null;
+    for(String word: words) {
+    	if(longestWord==null||word.length()>longestWord.length()) {
+    		longestWord= word;
+    		
+    	}
+    }
+    return longestWord;
+    }
+
+public static boolean containsSOS(List<String> morseCode) {
+for (int i = 0; i < morseCode.size(); i++) {
+	if(morseCode.get(i).equals(" ... --- ... ")) {
+		return true;
+	}
 }
+	return false;
+}
+
+public static List<Double> sortScores(List<Double> results) {
+        int n = results.size();  
+       double temp = 0;  
+         for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(results.get(j-1)>results.get(j)){  
+                                 //swap elements  
+                                 temp = results.get(j-1);  
+                                 results.set(j-1, results.get(j));
+//                              results.add(j-1, results.get(j));
+                                 results.set(j, temp);
+//                                 results.add(j, temp);
+                         }  
+                          
+                 }  
+            
+         }  
+  return results;
+    }  
+}
+
+
 
 
 
